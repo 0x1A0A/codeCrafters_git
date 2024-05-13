005192d74c181726a0e9cfc2d3412c71babef160
0x1A0A <mask.monkey1994@gmail.com>
2024-05-13 19:41:35 +0700

    feat: hash object

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
-use std::io::BufReader;
-use std::io::Read;
+use std::io::prelude::*;
 
-use flate2::bufread::ZlibDecoder;
+use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::Digest;
+use sha1::Sha1;
 
 fn git_init() {
     fs::create_dir(".git").unwrap();
@@ -22,8 +23,7 @@ fn cat_file(args: Vec<String>) {
     let value = fs::File::open(format!(".git/objects/{}/{}", prefix, &object[2..]));
     match value {
         Ok(data) => {
-            let reader = BufReader::new(data);
-            let mut d = ZlibDecoder::new(reader);
+            let mut d = ZlibDecoder::new(data);
             let mut s = String::new();
             d.read_to_string(&mut s).unwrap();
 
@@ -37,6 +37,40 @@ fn cat_file(args: Vec<String>) {
     }
 }
 
+fn hash_object(args: Vec<String>) {
+    let _option = &args[2];
+    let file = &args[3];
+    let content = fs::read_to_string(file).unwrap();
+    let data = format!("blob {}\0{}", content.len(), content);
+
+    let mut hash = Sha1::new();
+    hash.update(data.as_bytes());
+
+    let result = hash.finalize();
+    let hash_str = result
+        .iter()
+        .map(|e| format!("{:02x}", e))
+        .collect::<Vec<_>>()
+        .join("");
+
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+
+    e.write_all(data.as_bytes()).unwrap();
+
+    let compressed = e.finish().unwrap();
+
+    let prefix = &hash_str[0..2];
+
+    let _ = fs::create_dir(format!(".git/objects/{}", &prefix));
+
+    let _ = fs::write(
+        format!(".git/objects/{}/{}", prefix, &hash_str[2..]),
+        compressed,
+    );
+
+    print!("{}", hash_str)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
@@ -45,6 +79,7 @@ fn main() {
             println!("Initialized git directory")
         }
         "cat-file" => cat_file(args),
+        "hash-object" => hash_object(args),
         _ => println!("unknown command: {}", args[1]),
     }
 }
