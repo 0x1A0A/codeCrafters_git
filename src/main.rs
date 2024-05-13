22424ac687bc3e385b537310a22eed8a88638f14
0x1A0A <mask.monkey1994@gmail.com>
2024-05-13 15:46:10 +0700

    refactor: move code to a function instead

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,44 @@ use std::io::Read;
 
 use flate2::bufread::ZlibDecoder;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        let _option = &args[2];
-        let object = &args[3];
-        let prefix = &object[0..2];
+fn git_init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+}
 
-        let value = fs::File::open(format!(".git/objects/{}/{}", prefix, &object[2..]));
-        match value {
-            Ok(data) => {
-                let reader = BufReader::new(data);
-                let mut d = ZlibDecoder::new(reader);
-                let mut s = String::new();
-                d.read_to_string(&mut s).unwrap();
+fn cat_file(args: Vec<String>) {
+    let _option = &args[2];
+    let object = &args[3];
+    let prefix = &object[0..2];
 
-                if let Some(i) = s.find("\u{000}") {
-                    print!("{}", &s[i + 1..])
-                }
-            }
-            Err(e) => {
-                println!("{}", e)
+    let value = fs::File::open(format!(".git/objects/{}/{}", prefix, &object[2..]));
+    match value {
+        Ok(data) => {
+            let reader = BufReader::new(data);
+            let mut d = ZlibDecoder::new(reader);
+            let mut s = String::new();
+            d.read_to_string(&mut s).unwrap();
+
+            if let Some(i) = s.find("\u{000}") {
+                print!("{}", &s[i + 1..])
             }
         }
-    } else {
-        println!("unknown command: {}", args[1])
+        Err(e) => {
+            println!("{}", e)
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "init" => {
+            git_init();
+            println!("Initialized git directory")
+        }
+        "cat-file" => cat_file(args),
+        _ => println!("unknown command: {}", args[1]),
     }
 }
