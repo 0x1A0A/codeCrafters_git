986fa0412a03d647708577de812aa35978170b7c
0x1A0A <mask.monkey1994@gmail.com>
2024-05-13 10:23:11 +0700

    feat: add git cat-file for blob objetcts

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::BufReader;
+use std::io::Read;
+
+use flate2::bufread::ZlibDecoder;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,6 +15,27 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        let _option = &args[2];
+        let object = &args[3];
+        let prefix = &object[0..2];
+
+        let value = fs::File::open(format!(".git/objects/{}/{}", prefix, &object[2..]));
+        match value {
+            Ok(data) => {
+                let reader = BufReader::new(data);
+                let mut d = ZlibDecoder::new(reader);
+                let mut s = String::new();
+                d.read_to_string(&mut s).unwrap();
+
+                if let Some(i) = s.find("\u{000}") {
+                    println!("{}", &s[i+1..])
+                }
+            }
+            Err(e) => {
+                println!("{}", e)
+            }
+        }
     } else {
         println!("unknown command: {}", args[1])
     }
