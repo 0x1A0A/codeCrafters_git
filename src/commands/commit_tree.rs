5e6b5414b148229e02afecff17e2896d321c38d2
0x1A0A <mask.monkey1994@gmail.com>
2024-05-16 12:11:42 +0700

    fix: save commit object

--- src/commands/commit_tree.rs
+++ src/commands/commit_tree.rs
@@ -1,7 +1,11 @@
 use chrono::Local;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::io::prelude::*;
+use std::fs;
 
+use sha1::{Digest, Sha1};
 use crate::objects::Kind;
+use flate2::{write::ZlibEncoder, Compression};
 
 #[derive(Debug)]
 pub struct Options {
@@ -28,8 +32,29 @@ pub fn invoke(hash: &str, options: Options) {
         content = format!("{content}\nparent {parent}");
     }
 
-    content = format!("{content}\n{author}\n{commiiter}\n\n{}", options.message);
+    content = format!("{content}\n{author}\n{commiiter}\n\n{}\n", options.message);
 
     let header = format!("{} {}\0", Kind::Commit, content.len());
-    println!("{header}{content}");
+
+    let mut hasher = Sha1::new();
+    hasher.update(header.clone());
+    hasher.update(content.clone());
+
+    let hash = hasher.finalize();
+    let hash = hex::encode(hash);
+
+
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(header.as_bytes()).unwrap();
+    e.write_all(content.as_bytes()).unwrap();
+    let compressed = e.finish().unwrap();
+
+    let _ = fs::create_dir(format!(".git/objects/{}", &hash[..2]));
+    fs::write(
+        format!(".git/objects/{}/{}", &hash[..2], &hash[2..]),
+        compressed,
+    )
+    .unwrap();
+
+    println!("{hash}");
 }
