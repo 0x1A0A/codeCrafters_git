2913154c87d731ea01379c337d9a94ee0516ca23
0x1A0A <mask.monkey1994@gmail.com>
2024-05-27 23:45:10 +0700

    refactor: implement packfile

--- src/git/packfile.rs
+++ src/git/packfile.rs
@@ -1,6 +1,33 @@
+use flate2::read::ZlibDecoder;
 use std::io::{Read, Seek, SeekFrom};
 
 use super::helpers;
+
+#[derive(Debug, PartialEq, Eq)]
+enum ObjType {
+    Blob,
+    Tree,
+    Commit,
+    Tag,
+}
+
+impl std::fmt::Display for ObjType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ObjType::Blob => write!(f, "blob"),
+            ObjType::Tree => write!(f, "tree"),
+            ObjType::Commit => write!(f, "commit"),
+            ObjType::Tag => write!(f, "tag"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum PackObjType {
+    Base(ObjType),
+    OFS,
+    REF,
+}
 
 fn headers(stream: &mut impl Read) -> std::io::Result<([u8; 4], u32, u32)> {
     let header = helpers::read_bytes(stream)?;
@@ -13,10 +40,67 @@ fn headers(stream: &mut impl Read) -> std::io::Result<([u8; 4], u32, u32)> {
     Ok((header, version, entries))
 }
 
-fn read_object(stream: &mut (impl Seek + Read), pos: usize) -> std::io::Result<()> {
-    stream.seek(SeekFrom::Start(pos as u64))?;
+fn extract_size_and_type(size_and_type: usize) -> (usize, u8) {
+    ((size_and_type & !0x7) >> 3, (size_and_type & 0x7) as u8)
+}
 
-    Ok(())
+fn read_object(
+    stream: &mut (impl Seek + Read),
+    offset: usize,
+) -> std::io::Result<(Vec<u8>, ObjType)> {
+    stream.seek(SeekFrom::Start(offset as u64))?;
+
+    let size_and_type = helpers::read_size(stream)?;
+    let (size, t) = extract_size_and_type(size_and_type);
+
+    let t = match t {
+        0b001 => PackObjType::Base(ObjType::Commit),
+        0b010 => PackObjType::Base(ObjType::Tree),
+        0b011 => PackObjType::Base(ObjType::Blob),
+        0b100 => PackObjType::Base(ObjType::Tag),
+        0b110 => PackObjType::OFS,
+        0b111 => PackObjType::REF,
+        x => panic!("{x:03b} is invalid type."),
+    };
+
+    let (content, content_type) = match t {
+        PackObjType::Base(base) => {
+            let current = stream.stream_position()?;
+            let mut content = Vec::new();
+            content.reserve_exact(size);
+            let consume = decompress(stream, &mut content)?;
+
+            stream.seek(SeekFrom::Start(current + consume as u64))?;
+
+            (content, base)
+        }
+        PackObjType::OFS => {
+            let negativeoffset = helpers::read_offset(stream)?;
+            let current = stream.stream_position()?;
+
+            let (base, base_type) = read_object(stream, offset - negativeoffset)?;
+            let mut content = Vec::new();
+            content.reserve_exact(size);
+
+            stream.seek(SeekFrom::Start(current))?;
+            let consume = decompress(stream, &mut content)?;
+            stream.seek(SeekFrom::Start(current + consume as u64))?;
+
+            (base, base_type)
+        }
+        PackObjType::REF => {
+            unimplemented!()
+        }
+    };
+
+    Ok((content, content_type))
+}
+
+fn decompress(stream: &mut impl Read, buffer: &mut Vec<u8>) -> std::io::Result<usize> {
+    let mut z = ZlibDecoder::new(stream);
+    z.read_exact(buffer)?;
+
+    Ok(z.total_in() as usize)
 }
 
 #[cfg(test)]
@@ -40,5 +124,12 @@ mod tests {
 
         let mut reader = BufReader::new(data.as_slice());
         let _ = headers(&mut reader);
+    }
+
+    #[test]
+    fn extract_file_type_and_size() {
+        let (size, obj_type) = super::extract_size_and_type(0b1101_0110);
+        assert_eq!(size, 0b11010);
+        assert_eq!(obj_type, 0b110);
     }
 }
