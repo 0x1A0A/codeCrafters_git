d7f5d208a360d6bdc65a3936a451dc54fa15161d
0x1A0A <mask.monkey1994@gmail.com>
2024-05-28 14:47:59 +0700

    feat: apply delta object

--- src/git/packfile.rs
+++ src/git/packfile.rs
@@ -73,8 +73,9 @@ pub fn read_object(
             stream.seek(SeekFrom::Start(current))?;
             let consume = decompress(stream, &mut content)?;
             stream.seek(SeekFrom::Start(current + consume as u64))?;
+            let content = process_delta(&mut content, &base)?;
 
-            (base, base_type)
+            (content, base_type)
         }
         PackObjType::REF => {
             unimplemented!()
@@ -93,6 +94,57 @@ pub fn headers(stream: &mut impl Read) -> std::io::Result<([u8; 4], u32, u32)> {
     assert_ne!(entries, 0);
 
     Ok((header, version, entries))
+}
+
+fn process_delta(delta: &mut Vec<u8>, base: &Vec<u8>) -> std::io::Result<Vec<u8>> {
+    let mut reader = delta.as_slice();
+    let _ = helpers::read_size(&mut reader)?;
+    let content_size = helpers::read_size(&mut reader)?;
+    let mut content: Vec<u8> = Vec::new();
+
+    loop {
+        if content.len() == content_size {
+            break;
+        }
+
+        let [instruction] = helpers::read_bytes::<1>(&mut reader)?;
+        if instruction & 0x80 != 0 {
+            let (offset, size) = read_offset_and_size(&mut reader, instruction)?;
+            let copy = &base[offset..offset + size];
+            let mut source = Vec::from(copy);
+            content.append(&mut source);
+        } else {
+            let size = instruction & 0x7f;
+            let mut source = vec![0; size as usize];
+            source.reserve_exact(size as usize);
+            reader.read_exact(&mut source)?;
+            content.append(&mut source);
+        }
+    }
+
+    Ok(content)
+}
+
+fn read_offset_and_size(stream: &mut impl Read, flag: u8) -> std::io::Result<(usize, usize)> {
+    let offset_flag = flag & 0xf;
+    let mut offset: usize = 0;
+    for i in 0..4 {
+        if offset_flag & (1 << i) != 0 {
+            let [buf] = helpers::read_bytes::<1>(stream)?;
+            offset |= (buf as usize) << i * 8;
+        }
+    }
+
+    let size_flag = (flag & 0x70) >> 4;
+    let mut size = 0;
+    for i in 0..3 {
+        if size_flag & (1 << i) != 0 {
+            let [buf] = helpers::read_bytes::<1>(stream)?;
+            size |= (buf as usize) << i * 8;
+        }
+    }
+
+    Ok((offset, size))
 }
 
 fn extract_size_and_type(size_and_type: usize) -> (usize, u8) {
@@ -136,5 +188,15 @@ mod tests {
         let (size, obj_type) = super::extract_size_and_type(0b110_101_0110);
         assert_eq!(size, 0b110_0110);
         assert_eq!(obj_type, 0b101);
+    }
+
+    #[test]
+    fn read_offset_and_size() {
+        let data = vec![0x10, 0x80, 0xff];
+        let (offset, size) =
+            super::read_offset_and_size(&mut data.as_slice(), 0b1_010_0101).unwrap();
+
+        assert_eq!(offset, 0x800010);
+        assert_eq!(size, 0xff00);
     }
 }
