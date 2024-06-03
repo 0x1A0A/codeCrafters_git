d51d3f850c6c1334902c860a577fe20cba9edfd9
0x1A0A <mask.monkey1994@gmail.com>
2024-06-03 23:23:56 +0700

    test: Tree parsing

--- src/git/tree.rs
+++ src/git/tree.rs
@@ -16,20 +16,72 @@ pub fn parse(stream: &mut impl Read) -> std::io::Result<Vec<TreeItem>> {
     let mut tree = Vec::new();
 
     loop {
-        let mut data = Vec::new();
-        let consume = reader.read_until(0x00, &mut data)?;
-
-        if consume == 0 {
-            break;
+        match take(&mut reader) {
+            Some(item) => tree.push(item),
+            None => break,
         }
-        let mode_name = CStr::from_bytes_with_nul(&data).unwrap();
-        let (mode, name) = mode_name.to_str().unwrap().split_once(' ').unwrap();
-        let mode = u32::from_str_radix(mode, 8).unwrap();
-        let name = name.to_string();
-        let mut hash = [0; 20];
-        reader.read_exact(&mut hash)?;
-        tree.push(TreeItem { mode, name, hash });
     }
 
     Ok(tree)
+}
+
+pub fn take(reader: &mut impl BufRead) -> Option<TreeItem> {
+    let mut data = Vec::new();
+
+    if let Ok(consume) = reader.read_until(0x00, &mut data) {
+        if consume == 0 {
+            return None;
+        }
+    }
+
+    let mode_name = CStr::from_bytes_with_nul(&data).unwrap();
+    let (mode, name) = mode_name.to_str().unwrap().split_once(' ').unwrap();
+
+    let mode = u32::from_str_radix(mode, 8).unwrap();
+    let name = name.to_string();
+
+    let mut hash = [0; 20];
+    if let Err(_) = reader.read_exact(&mut hash) {
+        panic!("Invlalid Tree format!");
+    }
+
+    Some(TreeItem { mode, name, hash })
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    #[test]
+    fn parse_tree_valid() {
+        let mut data = Vec::new();
+        data.append(&mut Vec::from("40000 test.txt\0"));
+        data.append(&mut hex::decode("aca49a24ef448129fc42e2fb0de2f95f0096d09c").unwrap());
+        data.append(&mut Vec::from("100644 README.MD\0"));
+        data.append(&mut hex::decode("aca49a24ef448129fc42e2fb0de2f95f0096d09c").unwrap());
+        let mut reader = BufReader::new(data.as_slice());
+
+        let result = super::take(&mut reader);
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert_eq!(format!("{:o}", result.mode), "40000");
+        assert_eq!(result.name, "test.txt");
+        let _ = super::take(&mut reader);
+        let result = super::take(&mut reader);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn pares_correct_size() {
+        let mut data = Vec::new();
+        data.append(&mut Vec::from("40000 test.txt\0"));
+        data.append(&mut hex::decode("aca49a24ef448129fc42e2fb0de2f95f0096d09c").unwrap());
+        data.append(&mut Vec::from("100644 README.MD\0"));
+        data.append(&mut hex::decode("aca49a24ef448129fc42e2fb0de2f95f0096d09c").unwrap());
+        let mut reader = BufReader::new(data.as_slice());
+
+        let result = super::parse(&mut reader);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().len(), 2);
+    }
 }
